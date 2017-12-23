adcb257f43e24f5a41c4a27df4feddd414167a04
Tong cangzhen <fumitzuki@gmail.com>
2017-12-23 17:43:04 +0800

    sign in add

--- app/src/main/java/com/example/ldjg/pigknowclient/LoginActivity.java
+++ app/src/main/java/com/example/ldjg/pigknowclient/LoginActivity.java
@@ -3,7 +3,10 @@ package com.example.ldjg.pigknowclient;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -26,6 +29,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -62,6 +66,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mProgressView;
     private View mLoginFormView;
 
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberPasswd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,6 +89,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
         });
+
+
+        pref= PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPasswd=(CheckBox)findViewById(R.id.remember_pass) ;
+        boolean isremember=pref.getBoolean("remember_password",false);
+        if(isremember){
+            String account=pref.getString("account","");
+            String passwd=pref.getString("password","");
+            mEmailView.setText(account);
+            mPasswordView.setText(passwd);
+            rememberPasswd.setChecked(true);
+        }
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
@@ -184,6 +204,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+
+            editor=pref.edit();
+            if(rememberPasswd.isChecked()){
+                editor.putBoolean("remember_password",true);
+                editor.putString("account",email);
+                editor.putString("password",password);
+            }
+            else {
+                editor.clear();
+            }
+            editor.apply();
+
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
@@ -324,7 +356,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
@@ -333,6 +365,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
