6f53f691aa3e0b45b74a62a42e59bf893fdea000
Tong cangzhen <fumitzuki@gmail.com>
2017-12-23 20:23:03 +0800

    shexiangtou meiyong!

---
+++ app/src/main/java/com/example/ldjg/pigknowclient/TakephotoActivity.java
@@ -0,0 +1,100 @@
+package com.example.ldjg.pigknowclient;
+
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.cjt2325.cameralibrary.JCameraView;
+import com.cjt2325.cameralibrary.listener.ClickListener;
+import com.cjt2325.cameralibrary.listener.ErrorListener;
+import com.cjt2325.cameralibrary.listener.JCameraListener;
+
+
+import java.io.File;
+
+public class TakephotoActivity extends AppCompatActivity {
+    JCameraView jCameraView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_takephoto);
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(option);
+        }
+        jCameraView = (JCameraView) findViewById(R.id.jcameraview);
+        jCameraView.setSaveVideoPath(Environment.getExternalStorageDirectory().getPath() + File.separator + "JCamera");
+        jCameraView.setFeatures(JCameraView.BUTTON_STATE_BOTH);
+        jCameraView.setMediaQuality(JCameraView.MEDIA_QUALITY_MIDDLE);
+        jCameraView.setErrorLisenter(new ErrorListener() {
+            @Override
+            public void onError() {
+                //打开Camera失败回调
+                Log.i("CJT", "open camera error");
+            }
+            @Override
+            public void AudioPermissionError() {
+                //没有录取权限回调
+                Log.i("CJT", "AudioPermissionError");
+            }
+        });
+
+        jCameraView.setJCameraLisenter(new JCameraListener() {
+            @Override
+            public void captureSuccess(Bitmap bitmap) {
+                //获取图片bitmap
+                Log.i("JCameraView", "bitmap = " + bitmap.getWidth());
+            }
+            @Override
+            public void recordSuccess(String url,Bitmap firstFrame) {
+                //获取视频路径
+                Log.i("CJT", "url = " + url);
+            }
+            //@Override
+            //public void quit() {
+            //    (1.1.9+后用左边按钮的点击事件替换)
+            //}
+        });
+//左边按钮点击事件
+        jCameraView.setLeftClickListener(new ClickListener() {
+            @Override
+            public void onClick() {
+                TakephotoActivity.this.finish();
+            }
+        });
+//右边按钮点击事件
+        jCameraView.setRightClickListener(new ClickListener() {
+            @Override
+            public void onClick() {
+            Toast.makeText(TakephotoActivity.this,"Right",Toast.LENGTH_SHORT).show();
+        }
+    });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        jCameraView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        jCameraView.onPause();
+    }
+}
